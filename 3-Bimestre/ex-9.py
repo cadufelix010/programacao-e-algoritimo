997e6d415ead7ec72e3de3b660982e121af3246f
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 23:08:44 -0300

    Simplificar exercício 9 para nível iniciante

--- 3-Bimestre/ex-9.py
+++ 3-Bimestre/ex-9.py
@@ -1,12 +1,11 @@
-contador = 1
+n = int(input('Digite um número: '))
 fatorial = 1
-
-n = int(input('Digite um número inteiro positivo: '))
+contador = 1
 
 if n >= 0:
     while contador <= n:
-        fatorial *= contador
-        contador += 1
-    print(f'Fatorial: {fatorial}')
+        fatorial = fatorial * contador
+        contador = contador + 1
+    print('Fatorial:', fatorial)
 else:
     print('Número inválido')
