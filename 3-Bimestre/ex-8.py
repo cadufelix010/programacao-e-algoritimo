2f033dbbd477f1e0135af5bdba5f51d1abb71be1
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 23:08:39 -0300

    Simplificar exercício 8 para nível iniciante

--- 3-Bimestre/ex-8.py
+++ 3-Bimestre/ex-8.py
@@ -1,17 +1,16 @@
+n = int(input('Digite um número positivo: '))
 divisor = 1
 quantidade = 0
-
-n = int(input('Digite um número inteiro positivo: '))
 
 if n > 0:
     while divisor <= n:
         if n % divisor == 0:
-            quantidade += 1
-        divisor += 1
+            quantidade = quantidade + 1
+        divisor = divisor + 1
 
     if quantidade == 2:
-        print(f'O número {n} é primo')
+        print('É primo')
     else:
-        print(f'O número {n} não é primo')
+        print('Não é primo')
 else:
     print('Número inválido')
