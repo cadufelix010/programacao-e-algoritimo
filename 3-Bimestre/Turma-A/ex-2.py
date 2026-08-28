412d9ed9348bc88502ff5aca72ca5a0d2b40f6a0
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 22:56:50 -0300

    Organizar exercícios do 3º bimestre

--- 3-Bimestre/Turma-A/ex-2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input('Escolha um número: '))
-num2 = int(input('Escolha outro número: '))
-if num1 > num2:
-    print(f'O {num1} é maior que o {num2}')
-elif num1 < num2:
-    print(f'O {num2} é maior que o {num1}')
-else:
-    print(f'Os números {num1} e {num2} são iguais')
