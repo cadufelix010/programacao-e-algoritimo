f337001dcbef6b54a8eccffc153979dee6ac7531
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 23:08:34 -0300

    Simplificar exercício 7 para nível iniciante

--- 3-Bimestre/ex-7.py
+++ 3-Bimestre/ex-7.py
@@ -1,18 +1,20 @@
-contador = 1
 positivos = 0
 negativos = 0
 zeros = 0
+contador = 1
 
 while contador <= 10:
-    n = int(input(f'Digite o {contador}º número: '))
-    if n > 0:
-        positivos += 1
-    elif n < 0:
-        negativos += 1
-    else:
-        zeros += 1
-    contador += 1
+    n = int(input('Digite um número: '))
 
-print(f'Quantidade de positivos: {positivos}')
-print(f'Quantidade de negativos: {negativos}')
-print(f'Quantidade de zeros: {zeros}')
+    if n > 0:
+        positivos = positivos + 1
+    elif n < 0:
+        negativos = negativos + 1
+    else:
+        zeros = zeros + 1
+
+    contador = contador + 1
+
+print('Positivos:', positivos)
+print('Negativos:', negativos)
+print('Zeros:', zeros)
