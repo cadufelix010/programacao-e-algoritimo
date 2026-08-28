58d12e1fc74a15d648b56dad5277e6ce539faf42
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 23:08:18 -0300

    Simplificar exercício 3 para nível iniciante

--- 3-Bimestre/ex-3.py
+++ 3-Bimestre/ex-3.py
@@ -1,9 +1,9 @@
-n = int(input('Escolha um número inteiro positivo: '))
+n = int(input('Digite um número positivo: '))
 contador = 1
 
 if n > 0:
     while contador <= n:
         print(contador)
-        contador += 1
+        contador = contador + 1
 else:
     print('Número inválido')
