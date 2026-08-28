55f1c3a55dd71bf3aba4f191edef5c33d908bf0b
cadufelix010 <carlos2019felix@gmail.com>
2026-08-28 11:47:34 -0300

    Renomear exercício 1 - C++

---
+++ 3-Bimestre/C++/numeros-positivo-negativo-zero.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Digite um numero: ";
+    cin >> n;
+
+    if (n > 0)
+        cout << "Positivo";
+    else if (n < 0)
+        cout << "Negativo";
+    else
+        cout << "Zero";
+
+    return 0;
+}
