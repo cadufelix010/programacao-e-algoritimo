9a90c687702202d3676f4b4c215efcd652fcc508
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 22:56:47 -0300

    Organizar exercícios do 3º bimestre

--- 3-Bimestre/Turma-A/ex-1.py
+++
@@ -1,7 +0,0 @@
-n = int(input('Escolha um número inteiro: '))
-if n > 0:
-    print(f'O número {n} é positivo')
-elif n < 0:
-    print(f'O número {n} é negativo')
-else:
-    print('O número é igual a 0')
