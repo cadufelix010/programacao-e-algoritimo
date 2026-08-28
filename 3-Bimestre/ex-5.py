0d882e6da3b10f7e8a00221b703c97bb5c24b6d1
cadufelix010 <carlos2019felix@gmail.com>
2026-08-27 23:08:25 -0300

    Simplificar exercício 5 para nível iniciante

--- 3-Bimestre/ex-5.py
+++ 3-Bimestre/ex-5.py
@@ -1,9 +1,9 @@
-contador = 1
 n = int(input('Digite um número de 1 a 10: '))
+contador = 1
 
 if n >= 1 and n <= 10:
     while contador <= 10:
-        print(f'{n} x {contador} = {n * contador}')
-        contador += 1
+        print(n, 'x', contador, '=', n * contador)
+        contador = contador + 1
 else:
     print('Número inválido')
