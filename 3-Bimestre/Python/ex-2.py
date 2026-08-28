d29a2b3365028356682442eba39478e36bd785e7
cadufelix010 <carlos2019felix@gmail.com>
2026-08-28 11:41:43 -0300

    Organizar Python do 3º bimestre

---
+++ 3-Bimestre/Python/ex-2.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print(n1, 'é maior')
+elif n2 > n1:
+    print(n2, 'é maior')
+else:
+    print('Os números são iguais')
