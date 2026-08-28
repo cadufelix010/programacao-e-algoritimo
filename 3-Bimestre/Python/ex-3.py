b9cd96f7f77b9962f92ef1412b1aeb84d7976e5f
cadufelix010 <carlos2019felix@gmail.com>
2026-08-28 11:51:44 -0300

    Remover arquivos antigos e manter nomes descritivos

--- 3-Bimestre/Python/ex-3.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Digite um número positivo: '))
-contador = 1
-
-if n > 0:
-    while contador <= n:
-        print(contador)
-        contador = contador + 1
-else:
-    print('Número inválido')
