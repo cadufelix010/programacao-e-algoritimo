0e20c4ca237c4f9cf87f35f3fbf6b6744c009a6d
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:09 -0300

    Revisa exercício do 3º bimestre - fatorial.cpp

--- 3-Bimestre/C++/fatorial.cpp
+++ 3-Bimestre/C++/fatorial.cpp
@@ -1,20 +1,29 @@
+/*
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 09 - Fatorial
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n, contador = 1, fatorial = 1;
-    cout << "Digite um numero: ";
-    cin >> n;
+    int numero;
+    int contador = 1;
+    long long fatorial = 1;
 
-    if (n >= 0) {
-        while (contador <= n) {
-            fatorial = fatorial * contador;
-            contador++;
-        }
-        cout << "Fatorial: " << fatorial;
-    } else {
-        cout << "Numero invalido";
+    cout << "Digite um numero inteiro maior ou igual a zero: ";
+    cin >> numero;
+
+    if (numero < 0) {
+        cout << "Numero invalido." << endl;
+        return 0;
     }
 
+    while (contador <= numero) {
+        fatorial *= contador;
+        contador++;
+    }
+
+    cout << "Fatorial: " << fatorial << endl;
     return 0;
 }
