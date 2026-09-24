875db2a13bd8dd02daa73875f6617c3f9cf7dbf5
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:03 -0300

    Revisa exercício do 3º bimestre - media-notas.cpp

--- 3-Bimestre/C++/media-notas.cpp
+++ 3-Bimestre/C++/media-notas.cpp
@@ -1,25 +1,31 @@
+/*
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 06 - Média de notas
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
-    float nota, soma = 0, media;
-    int contador = 0;
+    float nota;
+    float soma = 0;
+    int quantidade = 0;
 
-    cout << "Digite uma nota de 0 a 10: ";
+    cout << "Digite uma nota entre 0 e 10: ";
     cin >> nota;
 
     while (nota >= 0 && nota <= 10) {
-        soma = soma + nota;
-        contador++;
-        cout << "Digite outra nota ou um numero fora de 0 a 10 para parar: ";
+        soma += nota;
+        quantidade++;
+
+        cout << "Digite outra nota ou um valor invalido para encerrar: ";
         cin >> nota;
     }
 
-    if (contador > 0) {
-        media = soma / contador;
-        cout << "Media: " << media;
+    if (quantidade > 0) {
+        cout << "Media das notas: " << soma / quantidade << endl;
     } else {
-        cout << "Nenhuma nota valida";
+        cout << "Nenhuma nota valida foi digitada." << endl;
     }
 
     return 0;
