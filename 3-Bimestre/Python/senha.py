f22b4b7fa329eb26e5f176dc2cf2839e1dd109fb
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:32 -0300

    Revisa exercício do 3º bimestre - senha.py

--- 3-Bimestre/Python/senha.py
+++ 3-Bimestre/Python/senha.py
@@ -1,15 +1,24 @@
-senha_correta = '1234'
+"""
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 10 - Sistema simples de senha
+"""
+
+senha_correta = "1234"
 tentativas = 0
+acesso_liberado = False
 
 while tentativas < 3:
-    senha = input('Digite a senha: ')
+    senha = input("Digite a senha: ")
 
     if senha == senha_correta:
-        print('Acesso permitido')
+        acesso_liberado = True
         break
-    else:
-        print('Senha incorreta')
-        tentativas = tentativas + 1
 
-if tentativas == 3:
-    print('Acesso bloqueado')
+    print("Senha incorreta.")
+    tentativas += 1
+
+if acesso_liberado:
+    print("Acesso liberado.")
+else:
+    print("Acesso bloqueado.")
