5a601e1ba4f6fbc26eb00c3597d2b613396948fc
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:01 -0300

    Revisa exercício do 3º bimestre - tabuada.cpp

--- 3-Bimestre/C++/tabuada.cpp
+++ 3-Bimestre/C++/tabuada.cpp
@@ -1,18 +1,27 @@
+/*
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 05 - Tabuada com validação
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n, contador = 1;
-    cout << "Digite um numero de 1 a 10: ";
-    cin >> n;
+    int numero;
+    int contador = 1;
 
-    if (n >= 1 && n <= 10) {
-        while (contador <= 10) {
-            cout << n << " x " << contador << " = " << n * contador << endl;
-            contador++;
-        }
-    } else {
-        cout << "Numero invalido";
+    cout << "Digite um numero de 1 a 10: ";
+    cin >> numero;
+
+    while (numero < 1 || numero > 10) {
+        cout << "Valor invalido. Digite novamente: ";
+        cin >> numero;
+    }
+
+    while (contador <= 10) {
+        cout << numero << " x " << contador
+             << " = " << numero * contador << endl;
+        contador++;
     }
 
     return 0;
