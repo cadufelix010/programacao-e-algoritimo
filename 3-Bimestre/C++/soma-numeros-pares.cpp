718c99097427e27d0d17a973fa20fcb760eae414
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:57:59 -0300

    Revisa exercício do 3º bimestre - soma-numeros-pares.cpp

--- 3-Bimestre/C++/soma-numeros-pares.cpp
+++ 3-Bimestre/C++/soma-numeros-pares.cpp
@@ -1,21 +1,31 @@
+/*
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 04 - Soma dos números pares
+*/
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n, contador = 1, soma = 0;
-    cout << "Digite um numero positivo: ";
-    cin >> n;
+    int numero;
+    int contador = 1;
+    int soma = 0;
 
-    if (n > 0) {
-        while (contador <= n) {
-            if (contador % 2 == 0)
-                soma = soma + contador;
-            contador++;
-        }
-        cout << "Soma dos numeros pares: " << soma;
-    } else {
-        cout << "Numero invalido";
+    cout << "Digite um numero inteiro positivo: ";
+    cin >> numero;
+
+    if (numero <= 0) {
+        cout << "Numero invalido." << endl;
+        return 0;
     }
 
+    while (contador <= numero) {
+        if (contador % 2 == 0) {
+            soma += contador;
+        }
+        contador++;
+    }
+
+    cout << "Soma dos pares: " << soma << endl;
     return 0;
 }
