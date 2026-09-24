903f3ee4c55968463414133a02086ad53c4b9743
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:13 -0300

    Revisa exercício do 3º bimestre - numeros-positivo-negativo-zero.py

--- 3-Bimestre/Python/numeros-positivo-negativo-zero.py
+++ 3-Bimestre/Python/numeros-positivo-negativo-zero.py
@@ -1,8 +1,14 @@
-n = int(input('Digite um número: '))
+"""
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 01 - Número positivo, negativo ou zero
+"""
 
-if n > 0:
-    print('Positivo')
-elif n < 0:
-    print('Negativo')
+numero = int(input("Digite um número inteiro: "))
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
 else:
-    print('Zero')
+    print("O número é zero.")
