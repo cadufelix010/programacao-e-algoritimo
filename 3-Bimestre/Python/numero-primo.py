ac8ac9901f344345330f5b147ebb3e67ed39071c
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:28 -0300

    Revisa exercício do 3º bimestre - numero-primo.py

--- 3-Bimestre/Python/numero-primo.py
+++ 3-Bimestre/Python/numero-primo.py
@@ -1,16 +1,22 @@
-n = int(input('Digite um número positivo: '))
+"""
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 08 - Número primo
+"""
+
+numero = int(input("Digite um número inteiro positivo: "))
 divisor = 1
-quantidade = 0
+quantidade_divisores = 0
 
-if n > 0:
-    while divisor <= n:
-        if n % divisor == 0:
-            quantidade = quantidade + 1
-        divisor = divisor + 1
+if numero > 0:
+    while divisor <= numero:
+        if numero % divisor == 0:
+            quantidade_divisores += 1
+        divisor += 1
 
-    if quantidade == 2:
-        print('É primo')
+    if quantidade_divisores == 2:
+        print("O número é primo.")
     else:
-        print('Não é primo')
+        print("O número não é primo.")
 else:
-    print('Número inválido')
+    print("Número inválido.")
