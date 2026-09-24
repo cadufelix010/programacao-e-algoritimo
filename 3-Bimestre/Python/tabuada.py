5f0bb990c103ea6bde2d9af8ba8c3267b5b45b92
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:21 -0300

    Revisa exercício do 3º bimestre - tabuada.py

--- 3-Bimestre/Python/tabuada.py
+++ 3-Bimestre/Python/tabuada.py
@@ -1,9 +1,16 @@
-n = int(input('Digite um número de 1 a 10: '))
+"""
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 05 - Tabuada com validação
+"""
+
+numero = int(input("Digite um número de 1 a 10: "))
+
+while numero < 1 or numero > 10:
+    numero = int(input("Valor inválido. Digite novamente: "))
+
 contador = 1
 
-if n >= 1 and n <= 10:
-    while contador <= 10:
-        print(n, 'x', contador, '=', n * contador)
-        contador = contador + 1
-else:
-    print('Número inválido')
+while contador <= 10:
+    print(f"{numero} x {contador} = {numero * contador}")
+    contador += 1
