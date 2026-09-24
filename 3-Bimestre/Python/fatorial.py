1782892e10d1e70a9bf3b0ac4d9eff3280839865
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:58:30 -0300

    Revisa exercício do 3º bimestre - fatorial.py

--- 3-Bimestre/Python/fatorial.py
+++ 3-Bimestre/Python/fatorial.py
@@ -1,11 +1,18 @@
-n = int(input('Digite um número: '))
-fatorial = 1
-contador = 1
+"""
+Aluno: Carlos Eduardo Félix
+3º Bimestre
+Exercício 09 - Fatorial
+"""
 
-if n >= 0:
-    while contador <= n:
-        fatorial = fatorial * contador
-        contador = contador + 1
-    print('Fatorial:', fatorial)
+numero = int(input("Digite um número inteiro maior ou igual a zero: "))
+contador = 1
+fatorial = 1
+
+if numero >= 0:
+    while contador <= numero:
+        fatorial *= contador
+        contador += 1
+
+    print("Fatorial:", fatorial)
 else:
-    print('Número inválido')
+    print("Número inválido.")
